088a914ae9ede5961b760b3112e3fa971bbac66a
Joeri Vlieghe <joeri.vlieghe@volvo.com>
2025-03-04 15:58:04 +0100

    Fix: Update connection string

--- getTeams.ts
+++ getTeams.ts
@@ -4,7 +4,7 @@ import { Connection, Request } from 'tedious';
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
-    const connectionString = process.env.AZURE_SQL_CONNECTION_STRING;
+    const connectionString = "Server=tcp:gentcitizendev-sqlsrv.database.windows.net,1433;Initial Catalog=OMS_APP_Input;Persist Security Info=False;User ID=rootadmin;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     const tableName = 'dbo.Teams';
 
     if (!connectionString) {
